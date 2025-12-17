f8c087bd19a42a7bd819401d81f68f8e56781203
Junginseong <twin2688@naver.com>
2025-12-17 21:36:13 +0900

    메뉴 버튼 컴포넌트 제작

---
+++ src/components/MenuButton.tsx
@@ -0,0 +1,59 @@
+/* React & React Native */
+import { Image, StyleSheet, TouchableOpacity, View } from "react-native";
+import React from "react";
+
+/* Styles */
+import { CommonStyles } from "../common/container";
+import { Color } from "../common/colors";
+import { T16 } from "./Typography";
+
+const iconMap = {
+  album: require("../../assets/icons/album.png"),
+  promise: require("../../assets/icons/promise.png"),
+  story: require("../../assets/icons/story.png"),
+} as const;
+
+type MenuType = {
+  name: string;
+  icon: keyof typeof iconMap;
+};
+
+export default function MenuButton({ name, icon }: MenuType) {
+  return (
+    <TouchableOpacity style={[styles.tabButton, CommonStyles.shadow]}>
+      <View style={styles.iconBox}>
+        <Image
+          source={iconMap[icon as keyof typeof iconMap]}
+          style={styles.icon}
+        />
+      </View>
+      <T16 style={{ marginLeft: 10 }}>{name}</T16>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  tabButton: {
+    width: "30%",
+    height: 50,
+    backgroundColor: Color.Main300,
+    alignItems: "center",
+    flexDirection: "row",
+    borderRadius: 7,
+    padding: 5,
+  },
+
+  iconBox: {
+    backgroundColor: "#fff",
+    width: 40,
+    height: 40,
+    borderRadius: 40 / 2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  icon: {
+    width: 50,
+    height: 50,
+  },
+});
