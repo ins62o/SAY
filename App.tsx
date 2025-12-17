e8da267f60b1fb9a04aad6ff0c1761d219b22ebe
Junginseong <twin2688@naver.com>
2025-12-17 21:34:22 +0900

    expo font 설정

--- App.tsx
+++ App.tsx
@@ -2,10 +2,20 @@
 import BottomTabNavigator from "./src/navigations/Tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useFonts } from "expo-font";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    PretendardMedium: require("./assets/fonts/Pretendard-Medium.ttf"),
+    PretendardBold: require("./assets/fonts/Pretendard-Bold.ttf"),
+    memo: require("./assets/fonts/Memoment.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
