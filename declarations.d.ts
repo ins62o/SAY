a16a250921417ba7cdfb94f00febf5f75f828f04
Junginseong <twin2688@naver.com>
2026-01-03 20:53:40 +0900

    svg 경로 추가, 환영 이미지 추가

--- declarations.d.ts
+++ declarations.d.ts
@@ -3,6 +3,14 @@ declare module "*.png" {
   export default value;
 }
 
+declare module "*.svg" {
+  import React from "react";
+  import { SvgProps } from "react-native-svg";
+
+  const SvgComponent: React.FC<SvgProps>;
+  export default SvgComponent;
+}
+
 declare module "*.jpg" {
   const value: number;
   export default value;
